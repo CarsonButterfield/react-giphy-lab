8da941adab38fca4e3981fa61d9d3528f5de864a
carsonbutterfield <carsonbutterfield98@gmail.com>
2019-11-16 12:50:44 -0800

    hello world

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 // Developer TODO: Define App component defintion here
+import React, {Component} from 'react'
+
+class App extends Component{
+    constructor(props) {
+        super(props)
+        console.log('constructed');
+        this.state = {
+          page: 'home',
+        }
+      }
+
+render(){
+    console.log('rendered')
+    return(
+        <p>hello world </p>
+    )}
+}
+
+
+export default App
